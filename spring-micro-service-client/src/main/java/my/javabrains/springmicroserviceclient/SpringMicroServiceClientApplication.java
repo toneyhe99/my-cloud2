d8e4800c127f429c2db6a047bb017c1c71c6908b
hetoney <toney.he@utoronto.ca>
2020-12-18 05:18:39 -0500

    spring cloud config

--- spring-micro-service-client/src/main/java/my/javabrains/springmicroserviceclient/SpringMicroServiceClientApplication.java
+++ spring-micro-service-client/src/main/java/my/javabrains/springmicroserviceclient/SpringMicroServiceClientApplication.java
@@ -1,28 +1,10 @@
 package my.javabrains.springmicroserviceclient;
 
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
-
-/**
- * after start, should print out in console:
- *       name: catalog--Cloud--QA; desc: QA welcome to catalog--Cloud--QA
- */
-@Data
 @SpringBootApplication
 public class SpringMicroServiceClientApplication {
-    @Value("${app.name}")
-    private String appName;
-    @Value("${app.desc}")
-    private String appDesc;
-
-    @PostConstruct
-    public void showProperty(){
-        System.out.println("\n\n name: "+appName+"; desc: "+appDesc);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMicroServiceClientApplication.class, args);
---
+++ spring-micro-service-client/src/main/java/my/javabrains/springmicroserviceclient/controller/HomeResources.java
@@ -0,0 +1,34 @@
+package my.javabrains.springmicroserviceclient.controller;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * after start, should print out in console:
+ *       name: catalog--Cloud--QA; desc: QA welcome to catalog--Cloud--QA
+ * should show loaded properties if call uri :  "localhost:8088/properties/show",
+ */
+@Data
+@RestController
+@RequestMapping("/properties")
+public class HomeResources {
+    @Value("${app.name}")
+    private String appName;
+    @Value("${app.desc}")
+    private String appDesc;
+
+    @PostConstruct
+    public void showProperty(){
+        System.out.println("\n\n name: "+appName+"; desc: "+appDesc);
+    }
+
+    @GetMapping("/show")
+    public String showLoadedProperteis(){
+        return "Properties loaded:  app.name="+appName+", app.desc="+appDesc;
+    }
+}
